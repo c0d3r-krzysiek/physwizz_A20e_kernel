cc0c7f7a8d16d4ff37cf6b62c4d209bfcc455b32
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2020-11-14 01:47:11 +0900

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export CROSS_COMPILE=gcc_toolchain/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
 
 make exynos7885-a20e_defconfig
