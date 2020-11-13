fb3c34720e39071b97dd93c4e0acacefa1d2ac27
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2020-11-14 02:00:53 +0900

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,7 +3,7 @@ echo "make sure you installed git"
 export ARCH=arm64
 export CROSS_COMPILE=gcc_toolchain/bin/aarch64-linux-android-
 export ANDROID_MAJOR_VERSION=q
-git clone https://github.com/roynatech2544/aarch64-toolchain-gcc.git -b gcc-4.9.x
+git clone https://github.com/roynatech2544/aarch64-toolchain-gcc.git -b gcc-4.9.x gcc-toolchain
 
 make exynos7885-a20e_defconfig
 make -j64
